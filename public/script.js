569cc1f024b3342f193a56d2bec9eed8c904361e
datwalkerv <89921503+datwalkerv@users.noreply.github.com>
2023-09-17 14:14:44 +0200

    v1

---
+++ public/script.js
@@ -0,0 +1,41 @@
+const socket = io();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
+  const input = document.querySelector('#message-input');
+  const messages = document.querySelector('#messages');
+  const chat = document.querySelector('#chat');
+
+    const storedMessages = JSON.parse(localStorage.getItem('messages')) || null;
+    if(storedMessages) {
+        storedMessages.forEach((msg) => {
+            const li = document.createElement('li');
+            li.textContent = msg;
+            messages.appendChild(li);
+        });
+    }
+    
+  
+  if(!localStorage.getItem('username')) {
+    localStorage.setItem('username', prompt('What is your name?'));
+    location.reload();
+ } 
+    const username = localStorage.getItem('username');
+    socket.emit('chat message', `${username} has joined the chat`);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (input.value) {
+      socket.emit('chat message', `${username}: ${input.value}`);
+      input.value = '';
+    }
+  });
+  
+  socket.on('chat message', (msg) => {
+    const li = document.createElement('li');
+    li.textContent = msg;
+    let getMessages = JSON.parse(localStorage.getItem('messages')) || [];
+    localStorage.setItem('messages', JSON.stringify([...getMessages, li.textContent]));
+    messages.appendChild(li);
+  });
+});
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    io.emit('chat message', 'A user disconnected')
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
